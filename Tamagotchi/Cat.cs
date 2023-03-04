eff2767452253f3c0070a5a6a0f22e7f7842c7de
deminas <deminas@411D-14>
2023-03-04 12:10:39 +0700

    Добавьте файлы проекта.

---
+++ Tamagotchi/Cat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tamagotchi
+{
+    struct Fruit
+    {
+        public string type;
+        public int health;
+    }
+    
+    
+    class Cat
+    {
+        public string name;
+        public int hunger = 100;
+        public void Meow()
+        {
+            Console.WriteLine("meow! ");
+        }
+        public void Eat(Fruit f)
+        {
+            Console.WriteLine($"Cat eats {f.type}. {f.health} hp.\n");
+        }
+    }
+}
---
+++ Tamagotchi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tamagotchi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat { name = "Pushok", hunger = 5 };
+            cat.Meow();
+            Fruit fruitApple = new Fruit {type = "apple", health = 10 };
+            cat.Eat(fruitApple);
+            Fruit fruitOrange = new Fruit {type = "apple", health = 5 };
+            cat.Eat(fruitOrange);
+            DoMeal(cat, fruitApple);
+            DoMeal(cat, fruitApple);
+            DoMeal(cat, fruitOrange);
+        }
+        static void DoMeal(Cat c, Fruit f)
+        {
+            c.Eat(f);
+        }
+    }
+}
