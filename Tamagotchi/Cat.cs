91210410e7a886f0caa933d1424ee79bbfa60a00
McLov1n <112742660+McLov1ns@users.noreply.github.com>
2023-03-18 18:50:02 +0700

    Update Cat.cs

--- Tamagotchi/Cat.cs
+++ Tamagotchi/Cat.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -14,14 +14,32 @@ namespace Tamagotchi
     class Cat
     {
         public string name;
-        public int hunger = 100;
+        public int hunger;
         public void Meow()
         {
-            Console.WriteLine("meow! ");
+            Console.WriteLine("meow meow!");
         }
-        public void Eat(Fruit f)
+        public void Eat(Fruit f, Cat cat)
         {
-            Console.WriteLine($"Cat eats {f.type}. {f.health} hp.\n");
+
+            Console.WriteLine($"{name} eats {f.type}. {f.health} hp.");
+            hunger = hunger + f.health;
+        }
+        public void Play()
+        {
+            hunger = hunger - 5;
+            Console.WriteLine("Cat is playing...  Hunger is " + hunger);
+        }
+        public void Run()
+        {
+            hunger = hunger - 10;
+            Console.WriteLine("Cat is runing... Hunger is " + hunger);
+        }
+        public void Wait()
+        {
+            Random rnd = new Random();
+            hunger = hunger - rnd.Next(2, 4);
+
         }
     }
 }
