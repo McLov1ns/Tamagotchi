4633f6412a4cdb3a96ab20c7065b2e877fca8a2f
McLov1n <112742660+McLov1ns@users.noreply.github.com>
2023-03-18 18:50:32 +0700

    Update Program.cs

--- Tamagotchi/Program.cs
+++ Tamagotchi/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -10,17 +10,80 @@ namespace Tamagotchi
         {
             Cat cat = new Cat { name = "Pushok", hunger = 5 };
             cat.Meow();
-            Fruit fruitApple = new Fruit {type = "apple", health = 10 };
-            cat.Eat(fruitApple);
-            Fruit fruitOrange = new Fruit {type = "apple", health = 5 };
-            cat.Eat(fruitOrange);
-            DoMeal(cat, fruitApple);
-            DoMeal(cat, fruitApple);
-            DoMeal(cat, fruitOrange);
+            Fruit fruitApple = new Fruit { type = "apple", health = 10 };
+            Fruit fruitOrange = new Fruit { type = "orange", health = 5 };
+            while (true)
+            {
+                Console.WriteLine(
+                    "what we do with a cat? \n1)Do meal \n2)Go play \n3)Go run \n4)Meow! \n5)wait \n6)exit \n7)hunger");
+                if (cat.hunger < 5 || cat.hunger == 0)
+                {
+                    Console.WriteLine("CAT DIED =(");
+                    break;
+                }
+
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("what fruit give to cat? \n1)Apple \n2)Orange");
+                        int variaty = Convert.ToInt32(Console.ReadLine());
+                        if (variaty == 1)
+                        {
+                            DoMeal(cat, fruitOrange, cat);
+                        }
+
+                        if (variaty == 2)
+                        {
+                            DoMeal(cat, fruitApple, cat);
+                        }
+
+                        if (cat.hunger >= 50)
+                        {
+                            Console.WriteLine("Cat is full");
+                            cat.Play();
+                        }
+
+                        continue;
+                    case 2:
+                        cat.Run();
+                        if (cat.hunger <= 0)
+                        {
+                            cat.Meow();
+                        }
+
+                        continue;
+                    case 3:
+                        cat.Run();
+                        if (cat.hunger <= 0)
+                        {
+                            cat.Meow();
+                        }
+
+                        continue;
+                    case 4:
+                        cat.Meow();
+                        continue;
+                    case 5:
+
+                        cat.Wait();
+                        Console.WriteLine("Cat is waiting...." + cat.hunger + "Hunger waste");
+                        continue;
+                    case 6:
+                        Console.WriteLine("Bye Bye!!");
+                        break;
+                    case 7:
+                        Console.WriteLine(cat.hunger);
+                        continue;
+                }
+
+                break;
+            }
         }
-        static void DoMeal(Cat c, Fruit f)
+
+        static void DoMeal(Cat c, Fruit f, Cat cat)
         {
-            c.Eat(f);
+            c.Eat(f, cat);
         }
     }
 }
